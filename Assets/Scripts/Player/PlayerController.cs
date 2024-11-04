3edd5303f4db5e5cab554c3fca41562167b539bd
nightwalkzc <1615754169@qq.com>
2024-11-04 23:32:10 +0800

    Player的简单移动

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -18,16 +18,36 @@ public class PlayerController : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
+            //禁止对角线移动（去掉对角线力矩）
+            if(input.x != 0) input.y = 0;
+
             if(input != Vector2.zero)
             {
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
-            }
-        }
 
+                StartCoroutine(Move(targetPos));
+            }
+
+            
+        }
     }
 
-    
+    // 将玩家从当前位置移动到目标位置
+    IEnumerator Move(Vector3 targetPos)  // 代码例程
+    {
 
+        isMove = true;
+
+        while((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon) //判断：目标位置与当前位置的差值存在微小差别
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed*Time.deltaTime);
+            yield return null;  //yield是什么东西？
+        }
+        transform.position = targetPos;
+
+        isMove = false;
+
+    }
 }
